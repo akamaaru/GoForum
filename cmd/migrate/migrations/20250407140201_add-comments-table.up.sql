da21f5038a594a001d556ce1f810d15c61ba4ae8
Беренштейн Аркадий Игоревич <a.b.darvin350@gmail.com>
2025-04-07 17:19:05 +0300

    added migrations

---
+++ cmd/migrate/migrations/20250407140201_add-comments-table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS posts (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `post_id` INT UNSIGNED NOT NULL,
+    `user_id` INT UNSIGNED NOT NULL,
+    `text` VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (post_id) REFERENCES posts(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
